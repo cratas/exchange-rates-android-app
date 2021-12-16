4de87610019ea22a404ce5721e230da71122b68b
Petr Kratochvíl <56606404+cratas@users.noreply.github.com>
2021-12-16 13:06:32 +0100

    Update CNBAdapter.java

--- app/src/main/java/com/example/netactivity/CNBAdapter.java
+++ app/src/main/java/com/example/netactivity/CNBAdapter.java
@@ -13,19 +13,6 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-// TODO 1. - Aktualne funguje aplikace tak, ze se nacte a zobrazi v listu kody men ze souboru https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.xml
-// TODO 1. - Cilem je upravit zobrazovani tak aby se zobrazovalo: vlajka statu, nazev meny, kod statu, nazev statu a kurz.
-// TODO 1. - Kazdy radek listu je reprezentovan instancemi tridy Entry, v tento okamzik je ve tride Entry udaj pouze pro kod zeme
-// TODO 1. - V nastaveni aplikace muzete nastavit jestli se maji data stahovat pouze pres wifi
-//  |-----------|----------------------------------|
-//  | ImageView |          NAZEV MENY              |  
-//  |           |Kod                               | 
-//  |  vlajka   |Stat                              |
-//  |   statu   |Kurz                              |
-//  |           |                                  |
-//  |----------------------------------------------|
-//
-
 public class CNBAdapter extends ArrayAdapter<Entry>{
 
     Context context;
